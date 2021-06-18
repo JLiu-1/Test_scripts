e04b4624c816723d781eebf51926689d2f6ecb8d
meso272 <liujinyang1201@126.com>
2021-06-17 18:28:18 -0700

    1

--- parse_ckpt.py
+++ parse_ckpt.py
@@ -7,17 +7,17 @@ import torch.nn as nn
 
 checkpoint=sys.argv[1]
 output=sys.argv[2]
-statedict=torch.load(checkpoint)["state_dict"]
+statedict=torch.load(checkpoint,map_location='cpu')["state_dict"]
 array=np.zeros(8,dtype=np.float32)
 count=0
 for key in statedict:
     print(statedict[key])
     if count==0:
 
-        array[0:7]=statedict[key].detach().cpu().numpy()
+        array[0:7]=statedict[key].detach().numpy()
         count+=1
     else:
-        array[7]=statedict[key].detach().cpu().numpy()[0]
+        array[7]=statedict[key].detach().numpy()[0]
 print(array)
 array.tofile(output)
 
