81494583cc352952d08e5906264ae7af1616ebcc
meso272 <liujinyang1201@126.com>
2021-06-27 22:15:17 -0700

    1

--- lr.py
+++ lr.py
@@ -3,10 +3,13 @@ from sklearn.linear_model import LinearRegression
 from nyx import NYX
 path="/home/jliu447/lossycompression/NYX"
 field="baryon_density"
-ratio=1
-dataset=NYX(path,field,0,3,ratio=ratio,log=1)
+ratio=1000
+
+dataset=NYX(path,field,0,1,ratio=ratio,log=1)
+print("finished reading data")
 x=dataset.blocks
 y=dataset.regs.flatten()
+print("start regression")
 reg=LinearRegression(fit_intercept=False).fit(x, y)
 
 print(reg.coef_)
