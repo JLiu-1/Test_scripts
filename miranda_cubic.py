10c047b0c512e602bcb2d8cb51244ebc5680cac7
meso272 <liujinyang1201@126.com>
2021-07-07 14:52:53 -0700

    1

--- miranda_cubic.py
+++ miranda_cubic.py
@@ -50,7 +50,7 @@ class Miranda_cubic(Dataset):
                                 block[2]=array[x][y][z+1]
                                 block[3]=array[x][y][z+3]
                             rng=np.max(block)-np.min(block)
-                            if rng<=1e-4：
+                            if rng<=1e-4:
                                 continue
                         
                             if global_max!=None:
