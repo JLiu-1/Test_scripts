748a6feb4fda75c773394167ae5083386ec87c37
meso272 <liujinyang1201@126.com>
2021-10-05 12:03:53 -0700

    ddd

---
+++ heat_lr_test.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import sys
+import os
+
+input_folder=sys.argv[1]
+ckpt_file=sys.argv[2]
+start=int(sys.argv[3])
+end=int(sys.argv[4])
+size_x=200
+size_y=200
+average_mse=0
+ckpt=np.fromfile(ckpt_file)
+for i in range(start,end):
+	filename_x="%d.dat" % (i) 
+    filename_y="%d.dat" % (i+1) 
+           
+    filepath_x=os.path.join(input_folder,filename_x)
+    filepath_y=os.path.join(input_folder,filename_y)
+    array_x=np.fromfile(filepath_x,dtype=np.float32).reshape((size_x,size_y))
+    array_y=np.fromfile(filepath_y,dtype=np.float32).reshape((size_x,size_y))
+    array_predict=np.array(array_y,copy=True)
--- lr_heat.py
+++ lr_heat.py
@@ -10,7 +10,10 @@ print("finished reading data")
 x=dataset.blocks.astype(np.float64)
 y=dataset.regs.flatten().astype(np.float64)
 print("start regression")
-reg=LinearRegression(fit_intercept=False).fit(x, y)
+reg=LinearRegression(fit_intercept=True).fit(x, y)
 
 print(reg.coef_)
-reg.coef_.tofile("lr_heat.dat")
+print(reg.intercept_)
+a=reg.coef_
+a=np.concatenate( (a, reg.intercept_) ) 
+a.tofile("lr_heat.dat")
