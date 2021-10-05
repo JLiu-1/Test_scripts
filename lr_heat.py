4b662942fda3e308a130529261ca892436daa619
meso272 <liujinyang1201@126.com>
2021-10-05 12:12:38 -0700

    ddd

--- lr_heat.py
+++ lr_heat.py
@@ -3,9 +3,9 @@ from sklearn.linear_model import LinearRegression
 from heat import Heat
 path="/home/jliu447/lossycompression/Heat"
 
-ratio=100
+ratio=1
 
-dataset=Heat(path,20000,20500,200,200,ratio=ratio)
+dataset=Heat(path,20000,21000,200,200,ratio=ratio)
 print("finished reading data")
 x=dataset.blocks.astype(np.float64)
 y=dataset.regs.flatten().astype(np.float64)
@@ -15,5 +15,5 @@ reg=LinearRegression(fit_intercept=True).fit(x, y)
 print(reg.coef_)
 print(reg.intercept_)
 a=reg.coef_
-a=np.concatenate( (a, reg.intercept_) ) 
+a=np.concatenate( (a, np.array([reg.intercept_]) ) )
 a.tofile("lr_heat.dat")
