d494113f3e5b6b000acf2b750ce989d605863973
meso272 <liujinyang1201@126.com>
2021-10-06 15:42:59 -0700

    ddd

--- load_model_heat.py
+++ load_model_heat.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 #parser.add_argument('--dropout','-d',type=float,default=0)
 parser.add_argument('--actv','-a',type=str,default='no')
 parser.add_argument('--checkpoint','-c',type=str)
-
+parser.add_argument('--double','-d',type=int,default=0)
 
 
 args = parser.parse_args()
@@ -17,6 +17,8 @@ args = parser.parse_args()
 actv_dict={"no":nn.Identity,"sigmoid":nn.Sigmoid,"tanh":nn.Tanh}
 actv=actv_dict[args.actv]
 model=nn.Sequential(nn.Linear(9,1),actv())
+if args.double:
+    model=model.double()
 model.load_state_dict(torch.load(args.checkpoint)["state_dict"])
 model.eval()
 for name,parameters in model.named_parameters():
