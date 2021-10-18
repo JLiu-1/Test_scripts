0e1899fa13db32025053a1c7a3944099ddb9b766
meso272 <liujinyang1201@126.com>
2021-10-18 14:54:16 -0700

    111

--- heat.py
+++ heat.py
@@ -113,7 +113,8 @@ class Heat_Random(Dataset):
         for i in range(size):
             block=np.random.rand(3,3)
             reg=(block[0,1]+block[1,0]+block[1,2]+block[2,1])/4
-        
+            print(block)
+            print(reg)
             blocks.append(block)
                    
             regs.append(reg)
