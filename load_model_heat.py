a9828249d4ea7b611a5c250c092eb25fc30c12b0
meso272 <liujinyang1201@126.com>
2021-10-28 12:34:03 -0700

    111

--- load_model_heat.py
+++ load_model_heat.py
@@ -10,13 +10,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--actv','-a',type=str,default='no')
 parser.add_argument('--checkpoint','-c',type=str)
 parser.add_argument('--double','-d',type=int,default=0)
-
+parser.add_argument('--conv','-v',type=int,default=0)
 
 args = parser.parse_args()
 
 actv_dict={"no":nn.Identity,"sigmoid":nn.Sigmoid,"tanh":nn.Tanh}
 actv=actv_dict[args.actv]
-model=nn.Sequential(nn.Linear(9,1),actv())
+if args.conv:
+    layer=nn.Conv2d(1,1,3)
+else:
+    layer=nn.Linear(9,1)
+model=nn.Sequential(layer,actv())
 if args.double:
     model=model.double()
 model.load_state_dict(torch.load(args.checkpoint)["state_dict"])
@@ -27,6 +31,6 @@ for name,parameters in model.named_parameters():
     a=parameters.detach().numpy()
     print(a)
     print(np.sum(a))
-x=torch.tensor([[1.0,1,1,1,1,1,1,1,1]])
-y=model(x)
-print(y)
+#x=torch.tensor([[1.0,1,1,1,1,1,1,1,1]])
+#y=model(x)
+#print(y)
