aa4ea3ca5282a7250148c390eeff323534ce224d
meso272 <liujinyang1201@126.com>
2021-11-03 16:01:45 -0700

    111

--- train_heat.py
+++ train_heat.py
@@ -154,7 +154,7 @@ ratio=args.ratio
 
 max_epoch=args.epoch
 interval=args.save_interval
-path="/home/jliu447/lossycompression/Heat_256_100"
+path="/home/jliu447/lossycompression/Heat_rd100"
 #val_path="/home/jliu447/lossycompression/NYX"
 #maximum={"baryon_density":5.06394195556640625,"temperature":6.6796627044677734375,"dark_matter_density":4.1392154693603515625}
 #minimum={"baryon_density":-1.306397557258605957,"temperature":2.7645518779754638672,"dark_matter_density":-10}
@@ -191,13 +191,13 @@ else:
         summary(model,(9,))
     except:
         print("Failed to summary.")
-start=5000
-end=9000
+start=1000
+end=10000
 interval=10
 gmax=100
 gmin=0
-sizex=256
-sizey=256
+sizex=128
+sizey=128
 if args.random:
     if args.double:
         train_loader = DataLoader(
