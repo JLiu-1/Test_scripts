659375cfb9de32092b171c2401fc247c84b47bf5
meso272 <liujinyang1201@126.com>
2022-02-04 13:53:08 -0800

    111

--- lr.py
+++ lr.py
@@ -1,9 +1,17 @@
 import numpy as np 
 from sklearn.linear_model import LinearRegression
 from nyx import NYX
+import sys
 path="/home/jliu447/lossycompression/NYX"
 field="baryon_density"
-ratio=1
+if len(sys.argv)>=2:
+    ratio=int(sys.argv[1])
+else:
+    ratio=1
+if len(sys.argv)>=3:
+    outfile=sys.argv[2]
+else:
+    outfile="lr_b.dat"
 
 dataset=NYX(path,field,0,3,ratio=ratio,log=1)
 print("finished reading data")
