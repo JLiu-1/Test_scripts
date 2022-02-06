6c8dc903c73fab4ad1d06ead888c80f1e076a244
meso272 <liujinyang1201@126.com>
2022-02-06 12:39:53 -0800

    111

---
+++ nyx_l1.py
@@ -0,0 +1,34 @@
+import numpy as np 
+from sklearn.linear_model import LinearRegression,Lasso
+from scipy.optimize import minimize
+from nyx import NYX
+import sys
+path="/home/jliu447/lossycompression/NYX"
+field="baryon_density"
+if len(sys.argv)>=2:
+    level=int(sys.argv[1])
+else:
+    level=2
+if len(sys.argv)>=3:
+    ratio=int(sys.argv[2])
+else:
+    ratio=1
+if len(sys.argv)>=4:
+    outfile=sys.argv[3]
+else:
+    outfile="lr_b.dat"
+def fit(X, params):
+    return X.dot(params)
+
+
+def cost_function(params, X, y):
+    return np.sum(np.abs(y - fit(X, params)))
+dataset=NYX(path,field,0,3,level=level,ratio=ratio,log=1)
+print("finished reading data")
+x=dataset.blocks.astype(np.double)
+y=dataset.regs.astype(np.double).flatten()
+print("start regression")
+
+output = minimize(cost_function, x0=np.ones(x.shape[-1]), args=(x, y))
+print(output)
+np.array(output.x).tofile(outfile)
