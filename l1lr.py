7f16e66741988d03ad4f9def3e15aa394b3dc18c
meso272 <liujinyang1201@126.com>
2022-02-06 12:10:24 -0800

    111

--- l1lr.py
+++ l1lr.py
@@ -11,5 +11,5 @@ X = np.asarray([np.ones((100,)), np.arange(0, 100)]).T
 print(X.shape)
 y = 10 + 5 * np.arange(0, 100) + 25 * np.random.random((100,))
 
-output = minimize(cost_function, x0=np.array([5.,10.]), args=(X, y))
+output = minimize(cost_function, x0=np.array([5.,10.]).T, args=(X, y))
 print(output)
