25bf77799389c87f3b95c513257ae3d036e3597e
meso272 <liujinyang1201@126.com>
2022-02-06 12:03:05 -0800

    111

--- l1lr.py
+++ l1lr.py
@@ -10,5 +10,5 @@ def cost_function(params, X, y):
 X = np.asarray([np.ones((100,)), np.arange(0, 100)]).T
 y = 10 + 5 * np.arange(0, 100) + 25 * np.random.random((100,))
 
-output = minimize(cost_function, x0=np.ones(len(y)), args=(X, y))
+output = minimize(cost_function, x0=np.ones(2), args=(X, y))
 print(output)
