280a5ba432afbf4af34e6a4a960a39ff9456d87f
meso272 <liujinyang1201@126.com>
2022-02-06 12:01:51 -0800

    111

---
+++ l1lr.py
@@ -0,0 +1,14 @@
+import numpy as np 
+from scipy.optimize import minimize
+def fit(X, params):
+    return X.dot(params)
+
+
+def cost_function(params, X, y):
+    return np.sum(np.abs(y - fit(X, params)))
+
+X = np.asarray([np.ones((100,)), np.arange(0, 100)]).T
+y = 10 + 5 * np.arange(0, 100) + 25 * np.random.random((100,))
+
+output = minimize(cost_function, x0=np.ones(len(y)), args=(X, y))
+print(output)
