cfc66b60a86a49d995fe9642550838e3e08b51f9
Meso <liujinyang1201@126.com>
2022-02-09 13:02:44 -0800

    123

--- cesm_compress_il_coeff.py
+++ cesm_compress_il_coeff.py
@@ -90,7 +90,7 @@ for x in range(2*(level-1),size_x,2):
         #print(block)
         reg_x=block[:size]
         if args.noise:
-            reg_x+=error_bound*np.random.rand(size)
+            reg_x+=error_bound*np.random.rand(size)/2
         reg_y=block[size]
         reg_xs.append(reg_x)
         reg_ys.append(reg_y)
