57e2d2591cd5c88a2701305f9ff700e0375c0053
meso272 <liujinyang1201@126.com>
2022-02-10 22:14:21 -0800

    111

--- multilevel_selective_compress_2d.py
+++ multilevel_selective_compress_2d.py
@@ -271,8 +271,8 @@ while step>0:#currently no recursive lorenzo
     mean_l1_loss=best_absloss/best_preds.size
     #print(np.max(np.abs(array[0:last_x+1:step,0:last_y+1:step]-best_preds)))
     array[0:last_x+1:step,0:last_y+1:step]=best_preds
-    qs+=cur_qs
-    us+=cur_us
+    qs+=best_qs
+    us+=best_us
     #print(len(qs))
     print ("Level %d finished. Selected algorithm: %s. Mean prediction abs loss: %f." % (level,selected_algo,mean_l1_loss))
     step=step//2
