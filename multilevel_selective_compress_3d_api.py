a18aa8d4027ea4d9c2fbfe4aa242a5e41240967e
meso272 <liujinyang1201@126.com>
2022-02-13 15:21:21 -0800

    111

--- multilevel_selective_compress_3d_api.py
+++ multilevel_selective_compress_3d_api.py
@@ -225,7 +225,7 @@ sample_rate=0.05,min_sampled_points=10,random_access=False):#lorenzo:only check 
             md_reg_ys=[]
             for x in range(1,cur_size_x,2):
                 for y in range(1,cur_size_y,2):
-                    for z in range(startz,cur_size_z,2):
+                    for z in range(zstart,cur_size_z,2):
                         md_reg_xs.append(np.array([cur_array[x-1][y][z],cur_array[x+1][y][z],cur_array[x][y-1][z],cur_array[x][y+1][z]],dtype=np.float64))
                         md_reg_ys.append(cur_array[x][y][z])
                         md_res=LinearRegression(fit_intercept=True).fit(md_reg_xs, md_reg_ys)
@@ -235,7 +235,7 @@ sample_rate=0.05,min_sampled_points=10,random_access=False):#lorenzo:only check 
     
         for x in range(1,cur_size_x,2):
             for y in range(1,cur_size_y,2):
-                for z in range(startz,cur_size_z,2):
+                for z in range(zstart,cur_size_z,2):
                 
                     orig=cur_array[x][y][z]
                     if level>=min_coeff_level:
@@ -477,7 +477,7 @@ sample_rate=0.05,min_sampled_points=10,random_access=False):#lorenzo:only check 
             md_reg_ys=[]
             for x in range(1,cur_size_x,2):
                 for y in range(1,cur_size_y,2):
-                    for z in range(startz,cur_size_z,2):
+                    for z in range(zstart,cur_size_z,2):
                         md_reg_xs.append(np.array([cur_array[x-1][y][z],cur_array[x+1][y][z],cur_array[x][y-1][z],cur_array[x][y+1][z]],dtype=np.float64))
                         md_reg_ys.append(cur_array[x][y][z])
                         md_res=LinearRegression(fit_intercept=True).fit(md_reg_xs, md_reg_ys)
@@ -487,7 +487,7 @@ sample_rate=0.05,min_sampled_points=10,random_access=False):#lorenzo:only check 
     
         for x in range(1,cur_size_x,2):
             for y in range(1,cur_size_y,2):
-                for z in range(startz,cur_size_z,2):
+                for z in range(zstart,cur_size_z,2):
                 
                     orig=cur_array[x][y][z]
                     if level>=min_coeff_level:
