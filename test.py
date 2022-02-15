649678ac98f5acaefcc419ca11b849c44ec39c46
Meso <liujinyang1201@126.com>
2022-02-15 15:39:25 -0800

    123

--- test.py
+++ test.py
@@ -14,12 +14,13 @@ for x in range(3,lastx+1):
 	for y in range(3,lasty+1):
 		if x%2==0 and y%2==0:
 			continue
+		orig=array[x][y]
 		elif x%2==0:
-			ave_error+=(-a[x][y-3]+9*a[x][y-1]+9*a[x][y+1]-a[x][y+3])/16
+			ave_error+=abs((-a[x][y-3]+9*a[x][y-1]+9*a[x][y+1]-a[x][y+3])/16-orig)
 		elif y%2==0:
-			ave_error+=(-a[x-3][y]+9*a[x-1][y]+9*a[x+1][y]-a[x+3][y])/16
+			ave_error+=abs((-a[x-3][y]+9*a[x-1][y]+9*a[x+1][y]-a[x+3][y])/16-orig)
 		else:
-			ave_error+=(-a[x][y-3]+9*a[x][y-1]+9*a[x][y+1]-a[x][y+3])/32+(-a[x-3][y]+9*a[x-1][y]+9*a[x+1][y]-a[x+3][y])/32
+			ave_error+=abs((-a[x][y-3]+9*a[x][y-1]+9*a[x][y+1]-a[x][y+3])/32+(-a[x-3][y]+9*a[x-1][y]+9*a[x+1][y]-a[x+3][y])/32-orig)
 		count+=1
 
 b=ave_error/count
