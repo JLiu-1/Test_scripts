4dc4c1b5521a0450bd2740eafac1eb3f0c469663
meso272 <liujinyang1201@126.com>
2022-02-14 21:53:13 -0800

    111

--- multilevel_selective_compress_2d_api.py
+++ multilevel_selective_compress_2d_api.py
@@ -9,7 +9,7 @@ import math
 import random
 from utils import *
 
-def msc2d(array,error_bound,rate,maximum_rate,min_coeff_level,max_step,anchor_rate,rate_list=None,x_preded=False,y_preded=False,sz_interp=False,multidim_level=10,lorenzo=-1,\
+def msc2d(array,error_bound,rate,maximum_rate,min_coeff_level,max_step,anchor_rate,rate_list=None,x_preded=False,y_preded=False,sz3_interp=False,multidim_level=10,lorenzo=-1,\
 sample_rate=0.05,min_sampled_points=10,random_access=False,verbose=False):#lorenzo:only check lorenzo fallback with level no larger than lorenzo level
 
     size_x,size_y=array.shape
@@ -112,7 +112,7 @@ sample_rate=0.05,min_sampled_points=10,random_access=False,verbose=False):#loren
     #linear interp
         absloss=0
         selected_algo="none"
-        if level<=multidim_level or not sz_interp:
+        if level<=multidim_level or not sz3_interp:
             if level>=min_coeff_level:
                 reg_xs=[]
                 reg_ys=[]
@@ -937,7 +937,7 @@ if __name__=="__main__":
         rate_list=None
     
     array,qs,edge_qs,us,_=msc2d(array,error_bound,args.rate,args.maximum_rate,args.min_coeff_level,args.max_step,args.anchor_rate,rate_list=rate_list,x_preded=False,y_preded=False,\
-        sz_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=args.lorenzo_fallback_check,sample_rate=args.fallback_sample_ratio,min_sampled_points=100,random_access=False,verbose=True)
+        sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=args.lorenzo_fallback_check,sample_rate=args.fallback_sample_ratio,min_sampled_points=100,random_access=False,verbose=True)
     quants=np.concatenate( (np.array(edge_qs,dtype=np.int32),np.array(sum(qs,[]),dtype=np.int32) ) )
     unpreds=np.array(us,dtype=np.float32)
     array.tofile(args.output)
