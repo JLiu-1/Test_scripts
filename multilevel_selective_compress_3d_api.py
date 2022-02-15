d728dfd6f00de74b8b161b37030b6d6daf6c5f8c
meso272 <liujinyang1201@126.com>
2022-02-15 00:25:33 -0800

    111

--- multilevel_selective_compress_3d_api.py
+++ multilevel_selective_compress_3d_api.py
@@ -1495,7 +1495,7 @@ if __name__=="__main__":
     error_bound=args.error*(np.max(array)-np.min(array))
     max_level=int(math.log(args.max_step,2))
     rate_list=args.rlist
-    
+    print(rate_list)
     if rate_list!=-1:
         if isinstance(rate_list,float):
             rate_list=[rate_list]
