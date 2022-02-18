9e29dcc4adbd43e7bfa60e526c7e440b189c826c
Meso <liujinyang1201@126.com>
2022-02-18 15:26:13 -0800

    123

--- multilevel_selective_compress_3d_api.py
+++ multilevel_selective_compress_3d_api.py
@@ -1526,7 +1526,7 @@ if __name__=="__main__":
         block_num_x=(args.size_x-1)//args.max_step
         block_num_y=(args.size_y-1)//args.max_step
         block_num_z=(args.size_z-1)//args.max_step
-        steplength=int(math.sqrt(args.autotuning))
+        steplength=int(args.autotuning**(1/3))
         bestalpha=1
         bestbeta=1
         #bestpdb=0
@@ -1538,8 +1538,8 @@ if __name__=="__main__":
         tq_name="%s_tq.dat"%pid
         tu_name="%s_tu.dat"%pid
         max_step=args.max_step
-        for k,alpha in enumerate(alpha_list):
-            beta=beta_list[k]
+        for m,alpha in enumerate(alpha_list):
+            beta=beta_list[m]
             test_qs=[[] for i in range(max_level+1)]
             test_us=[]
             square_error=0
@@ -1551,32 +1551,34 @@ if __name__=="__main__":
             #print(themean)
             for i in range(0,block_num_x,steplength):
                 for j in range(0,block_num_y,steplength):
-                  
-                    x_start=max_step*i
-                    y_start=max_step*j
-                    x_end=x_start+max_step+1
-                    y_end=y_start+max_step+1
-                    #print(x_start)
-                    #print(y_start)
-                    cur_array=np.copy(array[x_start:x_end,y_start:y_end])
-                    curmax=np.max(cur_array)
-                    curmin=np.min(cur_array)
-                    if curmax>themax:
-                        themax=curmax
-                    if curmin<themin:
-                        themin=curmin
-                    cur_array,cur_qs,edge_qs,cur_us,_=msc2d(cur_array,error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
-                                            sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=-1,sample_rate=0.0,min_sampled_points=100,random_access=False,verbose=False,fix_algo="none")
-                    #print(len(cur_qs[max_level]))
-                    #print(len(test_qs[max_level]))
-                    for level in range(max_level+1):
-                        #print(level)
-                        test_qs[level]+=cur_qs[level]
-                    test_us+=cur_us
-                    #zero_square_error=np.sum((array[x_start:x_end,y_start:y_end]-themean*np.ones((max_step+1,max_step+1)) )**2)
-                    square_error+=np.sum((array[x_start:x_end,y_start:y_end]-cur_array)**2)
-                    
-                    element_counts+=(max_step+1)**2 
+                    for k in range(0,block_num_z,steplength):
+                        x_start=max_step*i
+                        y_start=max_step*j
+                        z_start=max_step*z
+                        x_end=x_start+max_step+1
+                        y_end=y_start+max_step+1
+                        z_end=z_start+max_step+1
+                        #print(x_start)
+                        #print(y_start)
+                        cur_array=np.copy(array[x_start:x_end,y_start:y_end,z_start:z:end])
+                        curmax=np.max(cur_array)
+                        curmin=np.min(cur_array)
+                        if curmax>themax:
+                            themax=curmax
+                        if curmin<themin:
+                            themin=curmin
+                        cur_array,cur_qs,edge_qs,cur_us,_=msc3d(cur_array,error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
+                                                sz_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=-1,sample_rate=0.0,min_sampled_points=100,random_access=False,verbose=False,fix_algo="none")
+                        #print(len(cur_qs[max_level]))
+                        #print(len(test_qs[max_level]))
+                        for level in range(max_level+1):
+                            #print(level)
+                            test_qs[level]+=cur_qs[level]
+                        test_us+=cur_us
+                        #zero_square_error=np.sum((array[x_start:x_end,y_start:y_end]-themean*np.ones((max_step+1,max_step+1)) )**2)
+                        square_error+=np.sum((array[x_start:x_end,y_start:y_end,z_start:z_end]-cur_array)**2)
+                        
+                        element_counts+=(max_step+1)**2 
             t_mse=square_error/element_counts
             #zero_mse=zero_square_error/element_counts
             psnr=20*math.log(themax-themin,10)-10*math.log(t_mse,10)
@@ -1627,15 +1629,15 @@ if __name__=="__main__":
                         y_end=y_start+max_step+1
                         #print(x_start)
                         #print(y_start)
-                        cur_array=np.copy(array[x_start:x_end,y_start:y_end])
+                        cur_array=np.copy(array[x_start:x_end,y_start:y_end,z_start:z_end])
                         curmax=np.max(cur_array)
                         curmin=np.min(cur_array)
                         if curmax>themax:
                             themax=curmax
                         if curmin<themin:
                             themin=curmin
-                        cur_array,cur_qs,edge_qs,cur_us,_=msc2d(cur_array,new_error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
-                                                sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=-1,sample_rate=0.0,min_sampled_points=100,random_access=False,verbose=False,fix_algo="none")
+                        cur_array,cur_qs,edge_qs,cur_us,_=msc3d(cur_array,new_error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
+                                                sz_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=-1,sample_rate=0.0,min_sampled_points=100,random_access=False,verbose=False,fix_algo="none")
                         #print(len(cur_qs[max_level]))
                         #print(len(test_qs[max_level]))
                         for level in range(max_level+1):
@@ -1643,7 +1645,7 @@ if __name__=="__main__":
                             test_qs[level]+=cur_qs[level]
                         test_us+=cur_us
                         #zero_square_error=np.sum((array[x_start:x_end,y_start:y_end]-themean*np.ones((max_step+1,max_step+1)) )**2)
-                        square_error+=np.sum((array[x_start:x_end,y_start:y_end]-cur_array)**2)
+                        square_error+=np.sum((array[x_start:x_end,y_start:y_end,z_start:z_end]-cur_array)**2)
                         
                         element_counts+=(max_step+1)**2 
                 t_mse=square_error/element_counts
