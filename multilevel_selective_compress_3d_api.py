c0e2df573c38f433a9cf6105e17bccd2055c66ea
Meso <liujinyang1201@126.com>
2022-02-22 11:16:40 -0800

    123

--- multilevel_selective_compress_3d_api.py
+++ multilevel_selective_compress_3d_api.py
@@ -9,15 +9,12 @@ import math
 import random
 from utils import *
 
-<<<<<<< HEAD
+
 def msc3d(array,error_bound,rate,maximum_rate,min_coeff_level,max_step,anchor_rate,rate_list=None,x_preded=False,\
     y_preded=False,z_preded=False,multidim_level=10,sz_interp=False,lorenzo=-1,\
 sample_rate=0.05,min_sampled_points=10,new_q_order=0,random_access=False,verbose=False,pred_check=False,\
 fix_algo="none",fix_algo_list=None,first_level=None,last_level=0,fake_compression=False):#lorenzo:only check lorenzo fallback with level no larger than lorenzo level
-=======
-def msc3d(array,error_bound,rate,maximum_rate,min_coeff_level,max_step,anchor_rate,rate_list=None,x_preded=False,y_preded=False,z_preded=False,multidim_level=10,sz_interp=False,lorenzo=-1,\
-sample_rate=0.05,min_sampled_points=10,new_q_order=0,selection_criteria="l1",random_access=False,verbose=False,pred_check=False,fix_algo="none",fix_algo_list=None,first_level=None,last_level=0,fake_compression=False):#lorenzo:only check lorenzo fallback with level no larger than lorenzo level
->>>>>>> f5694902d0c64e2031ac65f3a3f7466efc2618a0
+
 
     size_x,size_y,size_z=array.shape
     '''
