2fdc6011112d9ecceba27a2cd0096e8be364ad0a
Meso <liujinyang1201@126.com>
2022-02-23 12:28:17 -0800

    123

--- multilevel_selective_compress_3d_api_rebuild.py
+++ multilevel_selective_compress_3d_api_rebuild.py
@@ -10,7 +10,7 @@ import random
 from utils import *
 
 def msc3d(array,x_start,x_end,y_start,y_end,z_start,z_end,error_bound,rate,maximum_rate,min_coeff_level,max_step,anchor_rate,\
-    rate_list=None,x_preded=False,y_preded=False,z_preded=False,multidim_level=10,sz_interp=False,lorenzo=-1,\
+    rate_list=None,x_preded=False,y_preded=False,z_preded=False,multidim_level=-1,sz_interp=False,lorenzo=-1,\
 sample_rate=0.05,min_sampled_points=10,new_q_order=0,random_access=False,verbose=False,pred_check=False\
 ,fix_algo="none",fix_algo_list=None,first_level=None,last_level=0,first_order="block",fake_compression=False):#lorenzo:only check lorenzo fallback with level no larger than lorenzo level
 
@@ -173,7 +173,7 @@ sample_rate=0.05,min_sampled_points=10,new_q_order=0,random_access=False,verbose
         selected_algo="none"
         if fix_algo_list!=None:
             fix_algo=fix_algo_list[level]
-        if (fix_algo=="none" and level<=multidim_level) or fix_algo in ["linear","cubic","multidim"] or not sz_interp:
+        if (fix_algo=="none" and level>=multidim_level) or fix_algo in ["linear","cubic","multidim"] or not sz_interp:
             if fix_algo=="none" or fix_algo=="linear":
                 if new_q_order:# all new_q_order remain unmodified
                     q_array=np.zeros(array_slice.shape,dtype=np.int32)
@@ -2319,7 +2319,7 @@ if __name__=="__main__":
     parser.add_argument('--rlist',type=float,default=-1,nargs="+")
     parser.add_argument('--maximum_rate','-m',type=float,default=10.0)
     #parser.add_argument('--cubic','-c',type=int,default=1)
-    parser.add_argument('--multidim_level','-d',type=int,default=99)
+    parser.add_argument('--multidim_level','-d',type=int,default=-1)
     parser.add_argument('--lorenzo_fallback_check','-l',type=int,default=-1)
     parser.add_argument('--fallback_sample_ratio','-p',type=float,default=0.05)
 #parser.add_argument('--level_rate','-lr',type=float,default=1.0)
