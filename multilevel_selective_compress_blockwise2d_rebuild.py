179cdc0490cb098e15a6b0dbfc40f92bbecc57cc
meso272 <liujinyang1201@126.com>
2022-02-23 20:46:13 -0800

    111

--- multilevel_selective_compress_blockwise2d_rebuild.py
+++ multilevel_selective_compress_blockwise2d_rebuild.py
@@ -101,7 +101,7 @@ if __name__=="__main__":
                             
                             #left question: The predictor selection is separated on each block, which does not follow the real compression
                             #What about fix the prediction on SZ3_cubic?
-                        cur_array,cur_qs,edge_qs,cur_us,_,lsd=msc2d(cur_array,error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
+                        cur_array,cur_qs,edge_qs,cur_us,_,lsd=msc2d(cur_array,0,max_step+1,0,max_step+1,error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
                                                 sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=-1,sample_rate=0.0,min_sampled_points=100,random_access=False,verbose=False,fix_algo=args.fix_algo,fix_algo_list=None)
                             #print(len(cur_qs[max_level]))
                             #print(len(test_qs[max_level]))
@@ -165,7 +165,7 @@ if __name__=="__main__":
                                 #print(y_start)
                             cur_array=np.copy(array[x_start:x_end,y_start:y_end])
                                 
-                            cur_array,cur_qs,edge_qs,cur_us,_,lsd=msc2d(cur_array,new_error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
+                            cur_array,cur_qs,edge_qs,cur_us,_,lsd=msc2d(cur_array,0,max_step+1,0,max_step+1,new_error_bound,alpha,beta,9999,args.max_step,args.anchor_rate,rate_list=None,x_preded=False,y_preded=False,\
                                                     sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=-1,sample_rate=0.0,min_sampled_points=100,random_access=False,verbose=False,fix_algo=args.fix_algo,fix_algo_list=None)
                                 #print(len(cur_qs[max_level]))
                                 #print(len(test_qs[max_level]))
