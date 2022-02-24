b519ef951bc9332f9c0c44778453b239728f8a66
meso272 <liujinyang1201@126.com>
2022-02-23 23:25:01 -0800

    111

--- multilevel_selective_compress_blockwise2d_rebuild.py
+++ multilevel_selective_compress_blockwise2d_rebuild.py
@@ -322,7 +322,7 @@ if __name__=="__main__":
                     x_end=size_x if x_start+block_size>=last_x else x_start+block_size+1
                     y_end=size_y if y_start+block_size>=last_y else y_start+block_size+1
                     #print(x_start,y_start)
-                    cur_qs,cur_lorenzo_qs,cur_us,cur_selected=\
+                    cur_qs,cur_lorenzo_qs,cur_us,cur_selected,lsd=\
                     msc2d(array,x_start,x_end,y_start,y_end,error_bound,args.rate,args.maximum_rate,min_coeff_level,max_step,anchor_rate,\
                         rate_list=rate_list,sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=args.lorenzo_fallback_check,\
                         sample_rate=args.fallback_sample_ratio,min_sampled_points=10,x_preded=(x_start>0),y_preded=(y_start>0),random_access=False,fix_algo=args.fix_algo,\
