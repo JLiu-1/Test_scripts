5c5ad5547e6a41dc00f983803b515661048b166d
meso272 <liujinyang1201@126.com>
2022-02-28 22:00:26 -0800

    111

--- multilevel_selective_compress_2d_api_rebuild_new.py
+++ multilevel_selective_compress_2d_api_rebuild_new.py
@@ -1919,18 +1919,19 @@ if __name__=="__main__":
 
     else:
         fix_algo_list=None
-        if ((isinstance(rate_list,int) or isinstance(rate_list,float)) and  rate_list>0) or (isinstance(rate_list,list ) and rate_list[0]>0):
+    if ((isinstance(rate_list,int) or isinstance(rate_list,float)) and  rate_list>0) or (isinstance(rate_list,list ) and rate_list[0]>0):
 
-            if isinstance(rate_list,int) or isinstance(rate_list,float):
-                rate_list=[rate_list]
+        if isinstance(rate_list,int) or isinstance(rate_list,float):
+            rate_list=[rate_list]
 
-            while len(rate_list)<max_level:
-                rate_list.insert(0,rate_list[0])
-        else:
-            rate_list=None
+        while len(rate_list)<max_level:
+            rate_list.insert(0,rate_list[0])
+    else:
+        rate_list=None
     if args.rate<1:
         args.rate=1
         args.maximum_rate=1
+
     qs,edge_qs,us,_,lsd=msc2d(array,0,args.size_x,0,args.size_y,error_bound,args.rate,args.maximum_rate,args.min_coeff_level,args.max_step,args.anchor_rate,rate_list=rate_list,x_preded=False,y_preded=False,\
         sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=args.lorenzo_fallback_check,sample_rate=args.fallback_sample_ratio,min_sampled_points=100,random_access=False,verbose=True,fix_algo=args.fix_algo,fix_algo_list=fix_algo_list)
     quants=np.concatenate( (np.array(edge_qs,dtype=np.int32),np.array(sum(qs,[]),dtype=np.int32) ) )
