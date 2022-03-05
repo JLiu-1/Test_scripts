59b515080a5dc38bb0f4d8bdf96fead79093807c
meso272 <liujinyang1201@126.com>
2022-03-04 22:56:25 -0800

    111

--- multilevel_selective_compress_3d_api_rebuild.py
+++ multilevel_selective_compress_3d_api_rebuild.py
@@ -3141,8 +3141,8 @@ if __name__=="__main__":
 
 
 
-                        cur_qs,edge_qs,cur_us,_,lsd=msc2d(array,x_start,x_end,y_start,y_end,z_start,z_end,error_bound,args.rate,args.maximum_rate,args.min_coeff_level,args.max_step,args.anchor_rate,rate_list=rate_list,\
-                                                                        sz3_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=args.lorenzo_fallback_check,sample_rate=args.fallback_sample_ratio,\
+                        cur_qs,edge_qs,cur_us,_,lsd=msc3d(array,x_start,x_end,y_start,y_end,z_start,z_end,error_bound,args.rate,args.maximum_rate,args.min_coeff_level,args.max_step,args.anchor_rate,rate_list=rate_list,\
+                                                                        sz_interp=args.sz_interp,multidim_level=args.multidim_level,lorenzo=args.lorenzo_fallback_check,sample_rate=args.fallback_sample_ratio,\
                                                                         first_level=level,last_level=level,min_sampled_points=100,random_access=False,verbose=False,fix_algo=fix_algo,x_preded=(x_start>0),y_preded=(y_start>0),z_preded=(z_start>0))
                         qs+=sum(cur_qs,[])
 
