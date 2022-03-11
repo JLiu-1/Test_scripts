5986c6ad37d308dc10c5f385e76a1d1ad2504c1c
meso272 <liujinyang1201@126.com>
2022-03-11 13:40:07 -0800

    111

--- test_sz3new.py
+++ test_sz3new.py
@@ -24,6 +24,7 @@ if __name__=="__main__":
     parser.add_argument('--totaltuningrate',"-t",type=float,default=None)
     parser.add_argument('--cr_tuning',"-c",type=int,default=0)
     parser.add_argument('--linear_reduce',"-r",type=int,default=0)
+    parser.add_argument('--multidim',"-u",type=int,default=0)
     #parser.add_argument('--size_x','-x',type=int,default=1800)
     #parser.add_argument('--size_y','-y',type=int,default=3600)
     #parser.add_argument('--size_z','-z',type=int,default=512)
@@ -65,8 +66,8 @@ if __name__=="__main__":
     overall_psnr=np.zeros((num_ebs,1),dtype=np.float32)
     pid=os.getpid()
     
-    configstr="[GlobalSettings]\nCmprAlgo = %s \ntuningTarget = %s \n[AlgoSettings]\nautoTuningRate = %f \npredictorTuningRate= %f \nlevelwisePredictionSelection = %d \nmaxStep = %d \ninterpolationBlockSize = %d \ntestLorenzo= %d \nlinearReduce= %d \n" % \
-    (algo,tuning_target,args.abtuningrate,args.predtuningrate,args.levelwise,args.maxstep,blocksize,args.lorenzo,args.linear_reduce) 
+    configstr="[GlobalSettings]\nCmprAlgo = %s \ntuningTarget = %s \n[AlgoSettings]\nautoTuningRate = %f \npredictorTuningRate= %f \nlevelwisePredictionSelection = %d \nmaxStep = %d \ninterpolationBlockSize = %d \ntestLorenzo = %d \nlinearReduce = %d \nmultiDimInterp = %d \n" % \
+    (algo,tuning_target,args.abtuningrate,args.predtuningrate,args.levelwise,args.maxstep,blocksize,args.lorenzo,args.linear_reduce,args.multidim) 
     with open("%s.config" % pid,"w") as f:
         f.write(configstr)
 
