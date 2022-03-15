35fc08bd416be59ec9cfa4eb87b32c0c9739effc
meso272 <liujinyang1201@126.com>
2022-03-15 15:21:14 -0700

    111

--- test_zfp.py
+++ test_zfp.py
@@ -15,6 +15,7 @@ if __name__=="__main__":
     parser.add_argument('--dim','-d',type=int,default=2)
     parser.add_argument('--dims','-m',type=str,nargs="+")
     parser.add_argument('--large_eb','-l',type=int,default=0)
+    parser.add_argument('--ssim',"-s",type=int,default=0)
     #parser.add_argument('--config','-c',type=str,default=None)
     #parser.add_argument('--size_x','-x',type=int,default=1800)
     #parser.add_argument('--size_y','-y',type=int,default=3600)
@@ -26,11 +27,8 @@ if __name__=="__main__":
     datafiles=os.listdir(datafolder)
     datafiles=[file for file in datafiles if file.split(".")[-1]=="dat" or file.split(".")[-1]=="f32" or file.split(".")[-1]=="bin"]
     num_files=len(datafiles)
-    if args.large_eb:
-        ebs=[i*1e-2 for i in range(3,11)]
-    else:
-
-        ebs=[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-3 for i in range(10,21,5)]
+ 
+    ebs=[1e-5,5e-5]+[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-2 for i in range(1,11)]
     #ebs=[1e-4,1e-3,1e-2]
     num_ebs=len(ebs)
 
@@ -40,6 +38,9 @@ if __name__=="__main__":
     #nrmse=np.zeros((num_ebs,num_files),dtype=np.float32)
     overall_cr=np.zeros((num_ebs,1),dtype=np.float32)
     overall_psnr=np.zeros((num_ebs,1),dtype=np.float32)
+    ssim=np.zeros((num_ebs,num_files),dtype=np.float32)
+   
+    overall_ssim=np.zeros((num_ebs,1),dtype=np.float32)
     #algo=np.zeros((num_ebs,num_files),dtype=np.int32)
     pid=os.getpid()
     for i,eb in enumerate(ebs):
@@ -52,7 +53,7 @@ if __name__=="__main__":
             arr=np.fromfile(filepath,dtype=np.float32)
             rng=np.max(arr)-np.min(arr)
             abseb=rng*eb
-            comm="zfp -s -i %s -z %s.out -f -%d %s -a %f &>%s.txt" % (filepath,pid,args.dim," ".join(args.dims),abseb,pid)
+            comm="zfp -s -i %s -o %s.out -f -%d %s -a %f &>%s.txt" % (filepath,pid,args.dim," ".join(args.dims),abseb,pid)
             print(comm)
             os.system(comm)
             with open("%s.txt"%pid,"r") as f:
@@ -67,7 +68,19 @@ if __name__=="__main__":
                 cr[i][j]=r 
                 psnr[i][j]=p
                 overall_psnr[i]+=n**2
-            os.system("rm -f %s.dat;rm -f %s.txt" % (pid,pid))
+            if args.ssim:
+
+                comm="calculateSSIM -f %s %s.out %s" % (filepath,pid," ".join(args.dims))
+                try:
+                    with os.popen(comm) as f:
+                        lines=f.read().splitlines()
+                        print(lines)
+                        s=eval(lines[-1].split('=')[-1])
+                        ssim[i][j]=max(s,0)
+                except:
+                    ssim[i][j]=0
+
+            os.system("rm -f %s.out;rm -f %s.txt" % (pid,pid))
             print(datafile,eb,rng,abseb,r,p)
 
     overall_psnr=overall_psnr/num_files
@@ -85,6 +98,12 @@ if __name__=="__main__":
     psnr_df.to_csv("%s_psnr.tsv" % args.output,sep='\t')
     overall_cr_df.to_csv("%s_overall_cr.tsv" % args.output,sep='\t')
     overall_psnr_df.to_csv("%s_overall_psnr.tsv" % args.output,sep='\t')
+    if (args.ssim):
+        overall_ssim=np.mean(ssim,axis=1)
+        ssim_df=pd.DataFrame(ssim,index=ebs,columns=datafiles)
+        overall_ssim_df=pd.DataFrame(overall_ssim,index=ebs,columns=["overall_ssim"])
+        ssim_df.to_csv("%s_ssim.tsv" % args.output,sep='\t')
+        overall_ssim_df.to_csv("%s_overall_ssim.tsv" % args.output,sep='\t')
    
                
         
