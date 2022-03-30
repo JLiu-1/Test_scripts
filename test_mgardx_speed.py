15e6e0a3c85d3530d095d6602dc5762c11a1ff35
meso272 <liujinyang1201@126.com>
2022-03-29 18:28:20 -0700

    111

--- test_mgardx_speed.py
+++ test_mgardx_speed.py
@@ -59,6 +59,7 @@ if __name__=="__main__":
             
             with os.popen(comm) as f:
                 lines=f.read().splitlines()
+                print(lines)
                 ct=eval(lines[-8].split(':')[-1].split("s")[0])
                 dt=eval(lines[-5].split(':')[-1].split("s")[0])
                 c_speed[i]+=ct
