9daced79e53b4aa88074282aeccfcb485ad3e099
meso272 <liujinyang1201@126.com>
2022-08-20 15:47:04 -0700

    111

--- test_sperr.py
+++ test_sperr.py
@@ -63,12 +63,12 @@ if __name__=="__main__":
             arr=np.fromfile(filepath,dtype=np.float32)
             rng=np.max(arr)-np.min(arr)
             abseb=rng*eb
-            if args.dim==2 and abseb<1e-8:
-                abseb=1e-8
+            #if args.dim==2 and abseb<1e-8:
+            #    abseb=1e-8
             if args.dim==2:
                 comm="compressor_2d --show_stats --dims %s -o %s.sperr --pwe %s %s &>%s.txt;decompressor_2d -o %s.sperr.out %s.sperr >>%s.txt;compareData -f %s %s.sperr.out>>%s.txt" % (" ".join(args.dims),pid,str(abseb),filepath,pid,pid,pid,pid,filepath,pid,pid)
             else:
-                comm="compressor_3d --show_stats --dims %s -o %s.sperr --pwe %f %s &>%s.txt;decompressor_3d -o %s.sperr.out %s.sperr >>%s.txt;compareData -f %s %s.sperr.out>>%s.txt" % (" ".join(args.dims),pid,abseb,filepath,pid,pid,pid,pid,filepath,pid,pid)
+                comm="compressor_3d --show_stats --dims %s -o %s.sperr --pwe %s %s &>%s.txt;decompressor_3d -o %s.sperr.out %s.sperr >>%s.txt;compareData -f %s %s.sperr.out>>%s.txt" % (" ".join(args.dims),pid,str(abseb),filepath,pid,pid,pid,pid,filepath,pid,pid)
             print(comm)
             os.system(comm)
             with open("%s.txt"%pid,"r") as f:
