c5bb341e2ff4ad174bfcc3f2aeb355f03d31242d
meso272 <liujinyang1201@126.com>
2023-01-11 17:33:59 -0800

    test

--- test_sz3new_temp.py
+++ test_sz3new_temp.py
@@ -85,7 +85,7 @@ if __name__=="__main__":
     num_files=len(datafiles)
     if args.tuning_target!="cr":
         #ebs=[1e-5,5e-5]+[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-3 for i in range(10,21,5)]
-        ebs=[2e-3,7e-3,0.01]
+        ebs=[6e-4,7e-4,8e-4,9e-4,2e-3,3e-3,4e-3,5e-3,6e-3,9e-3,2e-2]
     else:
         ebs=[1e-4,1e-3,1e-2]
     #ebs=[i*1e-3 for i in range(1,10)]+[i*1e-3 for i in range(10,21,5)]
