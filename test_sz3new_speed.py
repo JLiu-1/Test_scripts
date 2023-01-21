df1a06a0e2be8d3fddf0e70b687992cfa47346dc
meso272 <liujinyang1201@126.com>
2023-01-20 18:30:29 -0800

    speed

--- test_sz3new_speed.py
+++ test_sz3new_speed.py
@@ -52,6 +52,10 @@ if __name__=="__main__":
     parser.add_argument('--sperr',type=int,default=-1)
     parser.add_argument('--conditioning',type=int,default=0)
     parser.add_argument('--fixwave',type=int,default=0)
+    parser.add_argument('--wavetest',type=int,default=1)
+    parser.add_argument('--pybind',type=int,default=1)
+
+
     
 
     args = parser.parse_args()
@@ -97,10 +101,10 @@ if __name__=="__main__":
     configstr="[GlobalSettings]\nCmprAlgo = %s \ntuningTarget = %s \n[AlgoSettings]\nautoTuningRate = %f \npredictorTuningRate= %f \nlevelwisePredictionSelection = %d \nmaxStep =\
      %d \ninterpBlockSize = %d \ntestLorenzo = %d \nlinearReduce = %d \nmultiDimInterp = %d \nsampleBlockSize = %d \nprofiling = %d \nfixBlockSize = %d \nalpha = %f \nbeta = \
      %f \npdTuningAbConf = %d \npdAlpha = %d \npdBeta = %d \npdTuningRealComp = %d \nlastPdTuning = %d \nabList = %d \nblockwiseSampleBlockSize = %d \ncrossBlock = \
-     %d \nsampleBlockSampleBlockSize = %d \nwavelet = %d\nwavelet_rel_coeff = %f\npid = %s\nwaveletAutoTuning = %d\nvar_first = %d\nsperr = %d\nconditioning = %d\nfixWave = %d\n"\
+     %d \nsampleBlockSampleBlockSize = %d \nwavelet = %d\nwavelet_rel_coeff = %f\npid = %s\nwaveletAutoTuning = %d\nvar_first = %d\nsperr = %d\nconditioning = %d\nfixWave = %d\nwaveletTest = %d\npyBind = %d\n"\
      % (algo,tuning_target,args.abtuningrate,args.predtuningrate,args.levelwise,args.maxstep,blocksize,args.lorenzo,args.linear_reduce,args.multidim,args.sample_blocksize,\
         args.profiling,args.fixblock,args.alpha,args.beta,args.abconf,args.pda,args.pdb,args.pdreal,args.lastpdt,args.ablist,args.bsbs,args.cross,args.sbsbs,args.wavelet,\
-        args.wrc,pid,args.waveletautotuning,args.var_first,args.sperr,args.conditioning,args.fixwave) 
+        args.wrc,pid,args.waveletautotuning,args.var_first,args.sperr,args.conditioning,args.fixwave,args.wavetest,args.pybind) 
     with open("%s.config" % pid,"w") as f:
         f.write(configstr)
 
