c8eb9f9aee370d3d624e030ec948a15705a1290b
meso272 <liujinyang1201@126.com>
2023-05-08 12:46:30 -0700

    update

--- test_sz3new.py
+++ test_sz3new.py
@@ -88,10 +88,11 @@ if __name__=="__main__":
     if args.field!=None:
         datafiles=[file for file in datafiles if args.field in file]
     num_files=len(datafiles)
-    if args.tuning_target!="cr" and args.speed!=0:
-        ebs=[1e-5,5e-5]+[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-3 for i in range(10,21,5)]
-    else:
+    if args.tuning_target=="cr" or args.speed!=0:
         ebs=[1e-4,1e-3,1e-2]
+    else:
+       
+        ebs=[1e-5,5e-5]+[i*1e-4 for i in range(1,10)]+[i*1e-3 for i in range(1,10)]+[i*1e-3 for i in range(10,21,5)]
     #ebs=[i*1e-3 for i in range(1,10)]+[i*1e-3 for i in range(10,21,5)]
     
 
