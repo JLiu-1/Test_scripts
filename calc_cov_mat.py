4508eced6826d859580afd06ee34b676f8cb9831
meso272 <liujinyang1201@126.com>
2024-04-16 12:02:56 -0700

    upd

--- calc_cov_mat.py
+++ calc_cov_mat.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np 
-from sklearn.preprocessing import normalize
+#from sklearn.preprocessing import normalize
 
 cur_arg=1
 ifile=sys.argv[cur_arg]
@@ -28,10 +28,10 @@ if dim_count==3:
 mat_dims=input_array.shape
 
 if r_or_c: #c
-    input_array=normalize(input_array,axis=0)
-    output_array=np.matmul(np.transpose(input_array),input_array)
+    #input_array=normalize(input_array,axis=0)
+    output_array=np.matmul(np.transpose(input_array),input_array)/(mat_dims[0]-1)
 else:
-    input_array=normalize(input_array,axis=1)
-    output_array=np.matmul(input_array,np.transpose(input_array))
+    #input_array=normalize(input_array,axis=1)
+    output_array=np.matmul(input_array,np.transpose(input_array))/(mat_dims[1]-1)
 
 output_array.tofile(ofile)
